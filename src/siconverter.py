d29b1ee8eccf2211fd48157d64f72b57e16068cc
Nora <bnori97@gmail.com>
2019-10-20 15:10:31 +0200

    mikrom added

--- src/siconverter.py
+++ src/siconverter.py
@@ -1,4 +1,5 @@
 def siconverter(val, unit_in, unit_out):
-    SI = {'mm': 0.001, 'cm': 0.01, 'dm': 0.1, 'm': 1.0, 'km': 1000}
+    SI = {'mikrom': 1e-6, 'mm': 0.001, 'cm': 0.01,
+          'dm': 0.1, 'm': 1.0, 'km': 1000}
     output = val*SI[unit_in]/SI[unit_out]
     return output
