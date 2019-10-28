0e77a665529b5374dfd1fd9d279b073b3a2ba764
Gabor Rabatin <rabigabor@gmail.com>
2019-10-28 22:05:30 +0100

    Solved the anagram problem

--- src/is_anagram.py
+++ src/is_anagram.py
@@ -1,2 +1,8 @@
 def is_anagram(param1, param2):
-    print("TODO")
+    for letter in param1:
+        if letter not in param2:
+            return False
+    for letter in param2:
+        if letter not in param1:
+            return False
+    return True
