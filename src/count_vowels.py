7bd0474ecf13be8441f3be0a2c6c1959b1506f47
Gabor Rabatin <rabigabor@gmail.com>
2019-10-28 22:08:34 +0100

    Solved the Vowel Counter problem

--- src/count_vowels.py
+++ src/count_vowels.py
@@ -1,2 +1,4 @@
 def count_vowels(param):
-    print("TODO")
+    if type(param) is not str:
+        raise Exception("Please use string as input")
+    return len(list(filter(lambda x: x in ['a', 'e', 'i', 'o', 'u'], param)))
