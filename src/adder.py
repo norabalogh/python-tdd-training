171bf87fd950cd7965a7505b3684276f2abcdbed
Nóra <bnori97@gmail.com>
2020-10-11 16:27:42 +0000

    adder definition added

--- src/adder.py
+++ src/adder.py
@@ -1,4 +1,5 @@
 def add(num1, num2):
+    """A function that adds two numbers."""
     if(type(num1) == str or type(num2) == str):
         raise Exception("Input is sting. It should be int, double.")
     return num1 + num2
